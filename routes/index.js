fef77509ceed40345f5fdb6378a072bc8599e594
kmart2234 <21.kmart@gmail.com>
2018-06-05 04:45:00 -0700

    v1.0.1 - Adding replicas and resources to the docker-compose file

--- routes/index.js
+++ routes/index.js
@@ -4,21 +4,21 @@ const fibzy         = require('../helpers/fibzy');
 const router = express.Router();
 
 router.get('/:index', async(req, res) => {
-   try {
-       res.json(await fibzy(req.params.index))
-   }
-   catch(err){
-       res.status(400).send(err);
-   }
+    try {
+        res.json(await fibzy(req.params.index))
+    }
+    catch(err){
+        res.status(400).send(err);
+    }
 });
 
 router.post('/', async(req, res) => {
-   try {
-       res.json(await fibzy(req.body.index));
-   }
-   catch(err){
-       res.status(400).send(err);
-   }
+    try {
+        res.json(await fibzy(req.body.index));
+    }
+    catch(err){
+        res.status(400).send(err);
+    }
 });
 
 module.exports = router;
